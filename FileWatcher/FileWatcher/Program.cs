1c822f5091dd7951cc0964e93d1b8684dbf6e77c
Sebihind <74867534+Sebihind@users.noreply.github.com>
2021-05-28 09:04:54 +0200

    Add files via upload

---
+++ FileWatcher/FileWatcher/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FileWatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             var watcher = new FileSystemWatcher(@"X:\Programmierung\c_sharp\FileWatcher\FileWatcher\test");
+
+            watcher.NotifyFilter = NotifyFilters.LastAccess;
+
+            watcher.Changed += OnChanged;
+            watcher.Filter = "testfile.txt";
+            
+
+            watcher.IncludeSubdirectories = false;
+            watcher.EnableRaisingEvents = true;
+
+            Console.WriteLine("Press enter to exit");
+            Console.ReadLine();
+        }
+
+        private static void OnChanged(Object sender, FileSystemEventArgs e)
+        {
+            /*
+            if (e.ChangeType != WatcherChangeTypes.Changed)
+            {
+                return;
+            }
+            */
+            Console.WriteLine($"Changed: {e.FullPath}");
+        }
+    }
+}
